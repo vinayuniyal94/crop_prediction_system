102bc219402faa862f7c8b8e1c5ba5372324918d
vinayuniyal94 <uniyalvinay94@gmail.com>
2020-05-07 10:29:20 +0530

    Taking all 3 input from the user and savingin variable, now have to work with these variable for getting the prediction done

--- user_interface/views.py
+++ user_interface/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+drop1_dict = [
+    "Uttarakhand","Uttarpradesh",
+]
+
+drop2_dict = [
+    "Chamoli", "Haridwar", "Pauri",
+]
+
+drop3_dict = [
+    "Ravi", "Kharif", "Zaid"
+]
+
+
+
 # Create your views here.
 def predict(request):
-    state = request.POST['State']
+    state_value = request.POST['State']
+    selected_state = drop1_dict[int(state_value)]
+
+    district_value = request.POST['District']
+    selected_district = drop2_dict[int(district_value)]
+
+    season_value = request.POST['Season']
+    selected_season = drop3_dict[int(season_value)]
+
     context = {
-        "state":state
+        "state":selected_state,
+        "district":selected_district,
+        "season":selected_season,
     }
     return render(request, 'user_interface/prediction_result.html', context)
