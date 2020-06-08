cff48d219c74e05d4e68975984d35d0c0290b473
vinayuniyal94 <uniyalvinay94@gmail.com>
2020-06-08 17:45:31 +0530

    user login, signup, userDashboard html pages created

--- crop_prediction/views.py
+++ crop_prediction/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 
+# function calling home page in template directory
 def home(request):
     return render(request, "home.html")
 
+
+# function calling when user clicks checkProduction option in homepage
 def checkProduction(request):
     return render(request, "user_interface/user_input.html")
 
+# function calling when user clicks predictCrop option in homepage
 def predictCrop(request):
     return render(request, "user_interface/predictCrop_userInput.html")
+
+# function calling login page
+def userLogin(request):
+    return render(request, "user/userLogin.html")
+
+# function calling signup page
+def userSignup(request):
+    return render(request, "user/userSignup.html")
